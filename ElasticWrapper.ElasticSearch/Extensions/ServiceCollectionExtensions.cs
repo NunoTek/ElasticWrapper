cbcb54820cf3b3a3c51f9c9aa7051c16dc6f9bbf
Nuno Silva <NunoSilva@outlook.sk>
2025-03-10 16:11:10 +0100

    fix: github actions v4

---
+++ ElasticWrapper.ElasticSearch/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using ElasticWrapper.ElasticSearch.Base;
+using ElasticWrapper.ElasticSearch.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace ElasticWrapper.ElasticSearch.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddElasticWrapper<TModel, TModelFilter, TKey>(this IServiceCollection services, Action<ElasticOptions> configure)
+        where TModel : class, new()
+        where TModelFilter : class
+        where TKey : IEquatable<TKey>
+    {
+        services.AddOptions<ElasticOptions>().Configure(configure);
+        services.AddSingleton(x => x.GetRequiredService<IOptions<ElasticOptions>>().Value);
+
+        services.AddScoped<ElasticBaseRepository<TModel, TModelFilter, TKey>>();
+        services.AddScoped<ElasticRequestBuilder<TModel, TModelFilter>>();
+
+        return services;
+    }
+}
